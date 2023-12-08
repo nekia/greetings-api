289b1bce1879d03367264374a1c12c49092f8c42
Atsushi <nekiaiken@gmail.com>
2023-12-08 12:20:46 +0900

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestGreeting(t *testing.T) {
 	g := greeting()
-	should := "{\"greeting\":\"Hello Kubecon!\"}"
+	should := "{\"greeting\":\"Hello Qiita Advent Calendar 2023!\"}"
 
 	assert.Equal(t, should, g)
 }
